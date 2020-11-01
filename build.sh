b3760d0fa7a12f70d32a3cecb909f7701bc19ce1
MH <m.e.harms@t-online.de>
2020-11-01 07:20:30 +0100

    fix push

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ cd build
 cp -R ../app .
 VERSION=$(cat app/package.json | jq -r .version)
 docker build -t mahade70/aks-nsg-ingress-ban-ip:$VERSION .
-docker push mahade70/aks_acli_tools:$VERSION
+docker push mahade70/aks-nsg-ingress-ban-ip:$VERSION
 rm -rf app
